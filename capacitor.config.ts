17025d260a933429ca90985b5f557742c4f8972a
sravani <23f41a05d5@gmail.com>
2025-11-28 23:13:04 +0530

    fall alerts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,6 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'app.lovable.b95f0069d84e4d8ea84bece53bf35470',
-  appName: 'SafeFall AI',
+export default {
+  appId: 'com.deepu.safefallai',
+  appName: 'safefallai',
   webDir: 'dist',
-  server: {
-    url: 'https://b95f0069-d84e-4d8e-a84b-ece53bf35470.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  bundledWebRuntime: false,
 };
-
-export default config;
